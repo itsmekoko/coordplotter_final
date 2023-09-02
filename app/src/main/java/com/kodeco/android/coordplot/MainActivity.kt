33986e4b8328562162eb3bdb3e05ec3403059213
itsmekoko <cocotamarian@yahoo.com>
2023-09-03 00:05:56 +0300

    Feature - If applied my commit will add the two boxes

--- app/src/main/java/com/kodeco/android/coordplot/MainActivity.kt
+++ app/src/main/java/com/kodeco/android/coordplot/MainActivity.kt
@@ -3,12 +3,19 @@ package com.kodeco.android.coordplot
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.kodeco.android.coordplot.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -46,10 +53,21 @@ fun PlotSurfacePreview() {
 
 @Composable
 fun Map(xPercent: Float, yPercent: Float, modifier: Modifier = Modifier) {
-    // TODO fill out the square map here.
-    //  Create a Box composable with a size of 300.dp
-    //  and then create an inner Box composable
-    //  with a clip shape of CircleShape.
+    Box(
+        modifier = modifier
+            .size(300.dp)
+            .background(Color.Yellow)
+    ) {
+        Box(
+            modifier = Modifier
+                .offset(
+                    (xPercent * 300 - 18).dp, (yPercent * 300 - 18).dp
+                )
+                .size(50.dp)
+                .clip(CircleShape)
+                .background(Color.Blue)
+        )
+    }
 }
 
 @Preview(showBackground = true)
